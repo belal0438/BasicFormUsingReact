ce6a7b66246bf95cafbec540c0ab888ead9e57a3
Belal <mohammadvns121@gmail.com>
2023-09-05 22:38:51 +0530

    Working with ref

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+
 import "./InputForm.css";
 import Error from "./Error";
 import Wrapper from "./Helpers/Wrapper";
 
 const InputForm = (props) => {
-  const [inputUsername, setInputUsername] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
   // for Error
   const [error, setError] = useState();
 
-  const UsernameChangeHandler = (event) => {
-    setInputUsername(event.target.value);
-  };
-
-  const AgeChangeHandler = (event) => {
-    setInputAge(event.target.value);
-  };
-
   const SubmitHandler = (event) => {
     event.preventDefault();
+    // console.log(nameInputRef.current.value);
+    const enteredName = nameInputRef.current.value;
+    const enterUserage = ageInputRef.current.value;
+
     // now check validation
-    if (inputUsername.trim().length === 0 || inputAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enterUserage.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
@@ -29,7 +26,7 @@ const InputForm = (props) => {
     }
 
     // first convert the number to string
-    if (+inputAge < 1) {
+    if (+enterUserage < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid  age ( > 0).",
@@ -38,14 +35,13 @@ const InputForm = (props) => {
     }
     const dataObj = {
       id: Math.random().toString(),
-      name: inputUsername,
-      age: inputAge,
+      name: enteredName,
+      age: enterUserage,
     };
     // console.log(dataObj);
     props.onAddInput(dataObj);
-
-    setInputUsername("");
-    setInputAge("");
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -67,15 +63,11 @@ const InputForm = (props) => {
         <div className="form-control">
           <div className="form-control-input">
             <label>Username</label>
-            <input
-              type="text"
-              value={inputUsername}
-              onChange={UsernameChangeHandler}
-            />
+            <input type="text" ref={nameInputRef} />
           </div>
           <div className="form-control-input">
             <label>Age(Year)</label>
-            <input type="number" value={inputAge} onChange={AgeChangeHandler} />
+            <input type="number" ref={ageInputRef} />
           </div>
           <button type="submit">Add User</button>
         </div>
